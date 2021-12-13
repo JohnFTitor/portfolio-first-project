1693e8c3198675894f83293ecb85cee5c054090f
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2021-12-13 15:58:34 -0500

    Fix linters errors

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,12 @@
+function toggleMenu() {
+  document.querySelector('#menu').classList.toggle('menu-invisible');
+}
+
 const menuButton = document.querySelector('#menuButton');
 menuButton.addEventListener('click', toggleMenu);
 
 const closeButton = document.querySelector('#closeButton');
 closeButton.addEventListener('click', toggleMenu);
-
-function toggleMenu() {
-  document.querySelector('#menu').classList.toggle('menu-invisible');
-}
 
 const menuLinks = document.querySelectorAll('.menu-item a');
 menuLinks.forEach((link) => {
