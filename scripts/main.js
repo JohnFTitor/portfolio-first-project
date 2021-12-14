7ebae7632ab2edd8f8f1a47e8686475bf4e77eea
iamgaby <gaby.es.iaa@gmail.com>
2021-12-14 11:49:07 -0500

    Finish the renderCards function in main.js file

--- scripts/main.js
+++ scripts/main.js
@@ -41,26 +41,26 @@ took a standar dummy text.`, '', ['css', 'html', 'bootstrap', 'Ruby'], '', '');
 cards.push(featured);
 
 while (counter <= 6) {
-  const other = createCard('images/sampleproject.svg','Profesional Art Printing Data', 
-  `A daily selection of privately personalized reads; no accounts or sign-ups required. 
+  const other = createCard('images/sampleproject.svg', 'Profesional Art Printing Data',
+    `A daily selection of privately personalized reads; no accounts or sign-ups required. 
   has been the industry's standard`, '', ['html', 'bootstrap', 'Ruby'], '', '');
-  cards.push(other); 
+  cards.push(other);
 }
 
 function renderCards() {
-  cards.forEach( (cardObj) => {
+  cards.forEach((cardObj) => {
     const card = document.createElement('div');
-    if (cardObj.id === "card-1"){
+    if (cardObj.id === "card-1") {
       card.classList.add('card', 'featured');
     } else {
       card.classList.add('card', 'other');
     }
-    
+
     const img = document.createElement('img');
     img.setAttribute('src', cardObj.image);
     img.setAttribute('alt', '');
     card.appendChild(img);
-    
+
     const info = document.createElement('div');
     info.classList.add('info');
     card.appendChild(info);
@@ -73,11 +73,38 @@ function renderCards() {
     description.textContent = cardObj.shortDescription;
     info.appendChild(description);
 
-    
+    const container = document.createElement('ul');
+    container.classList.add('container', 'tags');
+    info.appendChild(container);
 
+    cardObj.tags.forEach((tag) => {
+      const litag = document.createElement('li');
+      litag.classList.add('textPlaceholder', 'tag');
+      container.appendChild(litag);
+
+      const alink = document.createElement('a');
+      alink.setAttribute('rel', 'noopener noreferrer');
+      alink.setAttribute('href', '#');
+      alink.setAttribute('aria-label', '');
+      alink.textContent = tag;
+      litag.appendChild(alink);
+    });
+
+    const cardButton = document.createElement('a');
+    cardButton.classList.add('interaction');
+    cardButton.setAttribute('rel', 'noopener noreferrer');
+    cardButton.setAttribute('href', `#${cardObj.id}`);
+    cardButton.setAttribute('aria-label', 'See Project');
+    cardButton.textContent = 'See Project';
+
+    if (cardObj.id === "card-1") {
+      info.appendChild(cardButton);
+    } else {
+      card.appendChild(cardButton);
+    }
 
     gridContainer.appendChild(card);
   })
-} 
+}
 
 renderCards();
